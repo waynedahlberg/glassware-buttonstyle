53c7f192005f355ba444e2286cb10cada4f2a13f
Wayne Dahlberg <waynefj40@gmail.com>
2024-09-25 15:18:20 -0600

    Initial Commit

---
+++ GlassButton/GlassButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassButtonApp.swift
+//  GlassButton
+//
+//  Created by Wayne Dahlberg on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GlassButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
